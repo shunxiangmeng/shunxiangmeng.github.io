825ad9da6a9ddebf2859858d2f9ba6c9cdc2e6cc
陈菇凉 <chen@chenguliangdeMacBook-Pro.local>
2020-09-26 23:54:16 +0800

    date函数支配safri浏览器

--- assistant/js/work.js
+++ assistant/js/work.js
@@ -1,3 +1,25 @@
+    /**
+ * 在Safari和IE8上执行 new Date('2017-12-8 11:36:45'); 会得到Invalid Date
+ * 本函数重写默认的Date函数，以解决其在Safari，IE8上的bug
+ */
+Date = function (Date) {
+    MyDate.prototype = Date.prototype;
+    return MyDate;
+
+    function MyDate() {
+        // 当只有一个参数并且参数类型是字符串时，把字符串中的-替换为/
+        if (arguments.length === 1) {
+        let arg = arguments[0];
+        if (Object.prototype.toString.call(arg) === '[object String]' && arg.indexOf('T') === -1) {
+            arguments[0] = arg.replace(/-/g, "/");
+            // console.log(arguments[0]);
+        }
+        }
+        let bind = Function.bind;
+        let unbind = bind.bind(bind);
+        return new (unbind(Date, null).apply(null, arguments));
+    }
+}(Date);
 
 function getAndDisplayWork()
 {
@@ -5,8 +27,9 @@ function getAndDisplayWork()
         results = data.results;
         //console.log(results);
         for (var i = 0; i < results.length; i++){
-            var updateTimeStr = results[i].updatedAt.replace(/T/i," ").replace(/Z/i,"");
-            var timestamp = new Date(Date.parse(updateTimeStr)).getTime();
+            var updateTimeStr = results[i].updatedAt.replace(/T/i," ").replace(/(.\d\d\dZ)/i,"");
+            //console.log(updateTimeStr);
+            var timestamp = new Date(updateTimeStr).getTime();
             results[i].timestamp = timestamp;
         }
         for (var i = 0; i < results.length; i++){
